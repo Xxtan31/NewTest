dae2fd82480467852a6b6f33dab6698df0fd9f44
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-07-16 22:03:04 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn-scripts').addEventListener('click', function() {
+    window.location.href = 'scripts.html';
+});
+
+document.querySelector('.btn-discord').addEventListener('click', function() {
+    window.location.href = 'https://discord.com';
+});
+
+document.querySelector('.btn-youtube').addEventListener('click', function() {
+    window.location.href = 'https://youtube.com';
+});
