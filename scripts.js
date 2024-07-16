558ad1764930997beddc20c07b9b8e8bc0088b1e
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-07-16 23:38:21 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-document.querySelector('.btn-scripts').addEventListener('click', function() {
-    window.location.href = 'scripts.html';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.btn-scripts').addEventListener('click', function() {
+        window.location.href = 'scripts.html';
+    });
 
-document.querySelector('.btn-discord').addEventListener('click', function() {
-    window.location.href = 'https://discord.com';
-});
+    document.querySelector('.btn-discord').addEventListener('click', function() {
+        window.location.href = 'https://discord.com';
+    });
 
-document.querySelector('.btn-youtube').addEventListener('click', function() {
-    window.location.href = 'https://youtube.com';
+    document.querySelector('.btn-youtube').addEventListener('click', function() {
+        window.location.href = 'https://youtube.com';
+    });
 });
